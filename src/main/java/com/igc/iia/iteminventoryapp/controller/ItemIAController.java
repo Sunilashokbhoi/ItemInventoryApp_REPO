74e123d157d01a407206ba1e209d27861b1ddf5c
Sunilashokbhoi <sunilbhoi606@gmail.com>
2023-08-20 20:14:21 +0530

    MODIFIED: Add Entity Class

--- src/main/java/com/igc/iia/iteminventoryapp/controller/ItemIAController.java
+++ src/main/java/com/igc/iia/iteminventoryapp/controller/ItemIAController.java
@@ -1,6 +1,6 @@
 package com.igc.iia.iteminventoryapp.controller;
 
-import com.igc.iia.iteminventoryapp.controller.Item;
+import com.igc.iia.iteminventoryapp.entity.Item;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
@@ -9,10 +9,32 @@ import java.util.ArrayList;
 @RequestMapping("/item/api")
 public class ItemIAController {
 
+    ArrayList<Item> itemArrayList = new ArrayList<>();
     @GetMapping("/itemname")
     public String name(){
         return "item name ";
     }
 
+    @PostMapping("/added")
+    public Item added(@RequestBody Item item){
+        itemArrayList.add(item);
+        return item;
+    }
+
+    @GetMapping("/getitem")
+    public Item getite(){
+        Item item = new Item();
+        item.setId(1);
+        item.setName("pen");
+        item.setDescription("Ball pen");
+        item.setPrice(5);
+        item.setQuantity(10);
+        item.setStatus("Availeble");
+        return item;
+    }
+    @GetMapping("/getallitems")
+    public ArrayList<Item> getallitems(){
+        return itemArrayList;
+    }
 }
 
